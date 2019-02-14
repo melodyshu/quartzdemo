b2300f11f2eec3d203fe5bae90bc77684e1a4a6f
huangzhimao1 <huangzhimao@jd.com>
2019-02-14 16:19:50 +0800

    JobExecutionContext

--- src/main/java/com/fxkt/HelloJob.java
+++ src/main/java/com/fxkt/HelloJob.java
@@ -3,6 +3,7 @@ package com.fxkt;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,5 +15,12 @@ public class HelloJob implements Job {
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String dtString=sdf.format(dt);
         System.out.println("正在执行任务,时间:"+dtString);
+        //获取jobDetail内容
+        JobKey jobKey= jobExecutionContext.getJobDetail().getKey();
+        System.out.println(jobKey.getName()+"--"+jobKey.getGroup());
+        System.out.println(jobExecutionContext.getJobDetail().getJobClass().getName());
+        System.out.println(jobExecutionContext.getJobDetail().getJobClass().getSimpleName());
+        //获取trigger内容
+
     }
 }
