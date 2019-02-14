b8020487e46b4a7f423aa33ee6c4d5e63c65acb5
huangzhimao1 <huangzhimao@jd.com>
2019-02-14 15:07:48 +0800

    输出job额外信息

--- src/main/java/com/fxkt/Main.java
+++ src/main/java/com/fxkt/Main.java
@@ -9,6 +9,9 @@ public class Main {
         JobDetail jobDetail= JobBuilder.newJob(HelloJob.class)
                 .withIdentity("job1","group1")
                 .build();
+        System.out.println("job名称:"+jobDetail.getKey().getName());
+        System.out.println("job组名称:"+jobDetail.getKey().getGroup());
+        System.out.println("任务类:"+jobDetail.getJobClass().getName());
         //定义触发器，马上执行，然后每5秒执行一次
         Trigger trigger= TriggerBuilder.newTrigger()
                 .withIdentity("trigger1","group1")
